30fcc078c443c3cef94f7786a7a306c6dc162107
Jatin Chawda <jztinchawda@gmail.com>
2022-05-08 00:30:20 +0200

    flake8 linting

--- house_prices/preprocess.py
+++ house_prices/preprocess.py
@@ -1,58 +1,72 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
-import sklearn  
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_log_error
 import joblib
+
 
 multiple_regression = joblib.load('../models/model.joblib', mmap_mode=None)
 ohe = joblib.load('../models/one_hot_encoder.joblib', mmap_mode=None)
 sc = joblib.load('../models/standard_scaler.joblib', mmap_mode=None)
 
+
 def select_clean_data(data):
-    X = data[['GarageArea', 'GarageCars', 'OverallQual','GrLivArea']]
+    X = data[['GarageArea', 'GarageCars', 'OverallQual', 'GrLivArea']]
     X = X.dropna().reset_index(drop=True)
     if 'SalePrice' in data.columns:
-        y = data['SalePrice'].values.reshape(-1,1)
+        y = data['SalePrice'].values.reshape(-1, 1)
     else:
-        return X 
-    return X,y
+        return X
+    return X, y
 
 
-def train_test_split_data(X_feature,y_feature,size,random):
-    X_train, X_test, y_train, y_test = train_test_split(X_feature, y_feature, test_size = size, random_state = random)
-    return X_train,X_test, y_train, y_test
+def train_test_split_data(X_feature, y_feature, size, random):
+    X_train, X_test, y_train, y_test = train_test_split(X_feature,
+                                                        y_feature,
+                                                        test_size=size,
+                                                        random_state=random)
+    return X_train, X_test, y_train, y_test
 
 
-  
-def encoding_with_one_hot_encoder(data,feature_1 ,feature_2):
-    fitting_encoder = ohe.fit(data[[feature_1 , feature_2]]).transform(data[[feature_1 , feature_2]])
-    col_names = ohe.get_feature_names_out(input_features = [feature_1 , feature_2])
-    encoder_df = pd.DataFrame(fitting_encoder,columns=col_names,index = data.index)
+def encoding_with_one_hot_encoder(data, feature_1, feature_2):
+    fitting_encoder = ohe.fit(data[[feature_1,
+                                    feature_2]]).transform(data[[feature_1,
+                                                                 feature_2]])
+    col_names = ohe.get_feature_names_out(input_features=[feature_1,
+                                                          feature_2])
+    encoder_df = pd.DataFrame(fitting_encoder, columns=col_names,
+                              index=data.index)
     data = data.join(encoder_df)
-    data = data.drop([feature_1,feature_2],axis=1)
-    return data
-
-def scaling_with_standard_scalar(data,feature_1,feature_2):
-    data[[feature_1, feature_2]] = sc.fit(data[[feature_1, feature_2]]).transform(data[[feature_1, feature_2]])
+    data = data.drop([feature_1, feature_2], axis=1)
     return data
 
 
-def train_linear_regression(X_data,y_data):
+def scaling_with_standard_scalar(data, feature_1, feature_2):
+    data[[feature_1,
+          feature_2]] = sc.fit(data[[feature_1,
+                                     feature_2]]).transform(data[[feature_1,
+                                                                  feature_2]])
+    return data
+
+
+def train_linear_regression(X_data, y_data):
     regression = multiple_regression.fit(X_data, y_data)
     return regression
-    
 
-def encoding_with_one_hot_encoder_with_transform(data,feature_1 ,feature_2):
-    fitting_encoder = ohe.transform(data[[feature_1 , feature_2]])
-    col_names = ohe.get_feature_names_out(input_features = [feature_1 , feature_2])
-    encoder_df = pd.DataFrame(fitting_encoder,columns=col_names,index = data.index)
+
+def encoder_with_transform(data, feature_1, feature_2):
+    fitting_encoder = ohe.transform(data[[feature_1, feature_2]])
+    col_names = ohe.get_feature_names_out(input_features=[feature_1,
+                                                          feature_2])
+    encoder_df = pd.DataFrame(fitting_encoder,
+                              columns=col_names,
+                              index=data.index)
     data = data.join(encoder_df)
-    data = data.drop([feature_1,feature_2],axis=1)
+    data = data.drop([feature_1, feature_2], axis=1)
     return data
 
-def scaling_with_standard_scalar_with_transform(data,feature_1,feature_2):
+
+def scaling_with_transform(data, feature_1, feature_2):
     data[[feature_1, feature_2]] = sc.transform(data[[feature_1, feature_2]])
     return data
 
@@ -62,6 +76,8 @@ def predict_data(data_to_predict):
     return y_pred
 
 
-def compute_rmsle(y_val: np.ndarray, y_pred: np.ndarray, precision: int = 2) -> float:
+def compute_rmsle(y_val: np.ndarray,
+                  y_pred: np.ndarray,
+                  precision: int = 2) -> float:
     rmsle = np.sqrt(mean_squared_log_error(y_val, y_pred))
     return round(rmsle, precision)
--- house_prices/train.py
+++ house_prices/train.py
@@ -1,14 +1,25 @@
 import pandas as pd
-from house_prices.preprocess import select_clean_data,train_test_split_data,encoding_with_one_hot_encoder,scaling_with_standard_scalar,train_linear_regression,encoding_with_one_hot_encoder_with_transform,scaling_with_standard_scalar_with_transform,predict_data,compute_rmsle
+from house_prices.preprocess import select_clean_data
+from house_prices.preprocess import train_test_split_data
+from house_prices.preprocess import encoding_with_one_hot_encoder
+from house_prices.preprocess import scaling_with_standard_scalar
+from house_prices.preprocess import train_linear_regression
+from house_prices.preprocess import encoder_with_transform
+from house_prices.preprocess import scaling_with_transform
+from house_prices.preprocess import predict_data
+from house_prices.preprocess import compute_rmsle
+
 
 def build_model(data: pd.DataFrame) -> dict[str, str]:
-    X,y = select_clean_data(data)
-    X_train,X_test, y_train, y_test = train_test_split_data(X,y,0.25,0)
-    X_train = encoding_with_one_hot_encoder(X_train,'GarageCars','OverallQual')
-    X_train = scaling_with_standard_scalar(X_train,'GarageArea','GrLivArea')
-    model = train_linear_regression(X_train,y_train)
-    X_test = encoding_with_one_hot_encoder_with_transform(X_test,'GarageCars','OverallQual')
-    X_test = scaling_with_standard_scalar_with_transform(X_test,'GarageArea','GrLivArea')
+    X, y = select_clean_data(data)
+    X_train, X_test, y_train, y_test = train_test_split_data(X,
+                                                             y, 0.25, 0)
+    X_train = encoding_with_one_hot_encoder(X_train,
+                                            'GarageCars', 'OverallQual')
+    X_train = scaling_with_standard_scalar(X_train, 'GarageArea', 'GrLivArea')
+    train_linear_regression(X_train, y_train)
+    X_test = encoder_with_transform(X_test, 'GarageCars', 'OverallQual')
+    X_test = scaling_with_transform(X_test, 'GarageArea', 'GrLivArea')
     y_predict = predict_data(X_test)
     compute_rmsle(y_test, y_predict)
-    return {'rsme' : compute_rmsle(y_test, y_predict)}
+    return {'rsme': compute_rmsle(y_test, y_predict)}
